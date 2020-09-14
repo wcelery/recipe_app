d4f8498d8012f1cbb1cf4d5640f5930ac26498a7
wcelery <horoschun.yura@yandex.ru>
2020-09-14 17:02:55 +0800

    Updates

---
+++ precache-manifest.f1fcefeb2c42d0ab0b35ac638b03fc23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45b6fa2849819c99e5c45af9ef59558",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "b9f162b33da99b826864",
+    "url": "/recipe-app/static/css/main.2f4fdd19.chunk.css"
+  },
+  {
+    "revision": "079c07660b9ff372d573",
+    "url": "/recipe-app/static/js/2.94c1c087.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.94c1c087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f162b33da99b826864",
+    "url": "/recipe-app/static/js/main.fbf58ac3.chunk.js"
+  },
+  {
+    "revision": "ce9b5b9c6618cac56af1",
+    "url": "/recipe-app/static/js/runtime-main.bffaae87.js"
+  }
+]);
